cb18bcafcce393973303b455234cb785a76ce429
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-03-15 21:16:52 +0700

    drop "back to manage" in content menu

--- views/mail-template/admin_view.php
+++ views/mail-template/admin_view.php
@@ -21,7 +21,6 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Mail Templates'), 'u
 $this->params['breadcrumbs'][] = $this->title;
 
 $this->params['menu']['content'] = [
-	['label' => Yii::t('app', 'Back To Manage'), 'url' => Url::to(['index']), 'icon' => 'table'],
 	['label' => Yii::t('app', 'Update'), 'url' => Url::to(['update', 'id'=>$model->template]), 'icon' => 'pencil'],
 	['label' => Yii::t('app', 'Delete'), 'url' => Url::to(['delete', 'id'=>$model->template]), 'htmlOptions' => ['data-confirm'=>Yii::t('app', 'Are you sure you want to delete this item?'), 'data-method'=>'post'], 'icon' => 'trash'],
 ];
