621ce6f3d326f487b11c622107319bb2ab051584
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-04-09 07:15:45 +0700

    add btn in content-menu

--- views/mail-template/admin_view.php
+++ views/mail-template/admin_view.php
@@ -21,8 +21,8 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Mail Templates'), 'u
 $this->params['breadcrumbs'][] = $this->title;
 
 $this->params['menu']['content'] = [
-	['label' => Yii::t('app', 'Update'), 'url' => Url::to(['update', 'id'=>$model->template]), 'icon' => 'pencil'],
-	['label' => Yii::t('app', 'Delete'), 'url' => Url::to(['delete', 'id'=>$model->template]), 'htmlOptions' => ['data-confirm'=>Yii::t('app', 'Are you sure you want to delete this item?'), 'data-method'=>'post'], 'icon' => 'trash'],
+	['label' => Yii::t('app', 'Update'), 'url' => Url::to(['update', 'id'=>$model->template]), 'icon' => 'pencil', 'htmlOptions' => ['class'=>'btn btn-primary btn-sm']],
+	['label' => Yii::t('app', 'Delete'), 'url' => Url::to(['delete', 'id'=>$model->template]), 'htmlOptions' => ['data-confirm'=>Yii::t('app', 'Are you sure you want to delete this item?'), 'data-method'=>'post', 'class'=>'btn btn-danger btn-sm'], 'icon' => 'trash'],
 ];
 ?>
 
