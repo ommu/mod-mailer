2929393a5b3b359351aa8ef2c602a4d9f1e087b3
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-11 11:47:03 +0700

    commit

--- views/mail-template/_search.php
+++ views/mail-template/_search.php
@@ -42,8 +42,10 @@ use yii\widgets\ActiveForm;
 		<?php echo $form->field($model, 'modified_search');?>
 
 		<div class="form-group">
-			<?php echo Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-			<?php echo Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+			<?php echo Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']); ?>
+			<?php echo Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']); ?>
 		</div>
+
 	<?php ActiveForm::end(); ?>
+
 </div>
