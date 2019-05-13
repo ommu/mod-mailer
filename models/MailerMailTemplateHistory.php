5479ab809600847848f9a6785175c3884ce1ae5a
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-13 11:05:44 +0700

    commit

--- models/MailerMailTemplateHistory.php
+++ models/MailerMailTemplateHistory.php
@@ -111,7 +111,7 @@ class MailerMailTemplateHistory extends \app\components\ActiveRecord
 
 		$this->templateColumns['_no'] = [
 			'header' => Yii::t('app', 'No'),
-			'class'  => 'yii\grid\SerialColumn',
+			'class' => 'yii\grid\SerialColumn',
 			'contentOptions' => ['class'=>'center'],
 		];
 		if(!Yii::$app->request->get('template')) {
